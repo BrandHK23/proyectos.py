d0be8062e92e3185f7e6547bb69d86976204dc2f
brandon <brandHK23@hotmail.com>
2022-11-27 16:05:01 -0600

    Se crea el programa mad lib

---
+++ mad_lib.py
@@ -0,0 +1,5 @@
+al = input("Ingrese un alimento (plural): \n");
+can = input("Ingrese una cantidad: \n");
+nom = input("Ingrese el nombre de alguien: \n");
+
+print("Parece que el cuerpo de", nom, "solo resiste", can, al, sep=" ");
