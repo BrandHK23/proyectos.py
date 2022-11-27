191a7c319e9815f849e90325da63a8239676a288
brandon <brandHK23@hotmail.com>
2022-11-27 14:23:47 -0600

    Se crea programa par o impar

---
+++ par_impar.py
@@ -0,0 +1,19 @@
+print("Bienvenido, este programa indica si un número es par o impar");
+r = str;
+intento = True
+while intento == True:
+    if r == "n":
+        intento == False;
+        print("Programa terminado...");
+        break
+    else:
+        var = int(input("Ingrese un numero entre 1 y 1000: "));
+        if (var % 2 == 0):
+            print("¡Es un número par!\n");
+            r = str(input("Quiere ingresar otro número?(s/n): "));
+        else:
+            print("¡Es un número impar!\n");
+            r = str(input("Quiere ingresar otro número?(s/n): "));
+            
+
+            
