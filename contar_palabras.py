fd9dc3ac015341b3c5339f52bd8906c9b1623cb1
brandon <brandHK23@hotmail.com>
2022-12-09 23:04:44 -0600

    se agrega el programa contar_palabras

---
+++ contar_palabras.py
@@ -0,0 +1,6 @@
+Pensamiento = input("¿En que estás pensando?")
+Pensamiento = Pensamiento.split()
+print(Pensamiento)
+count = len(Pensamiento)
+
+print("¡Muy bien, tu me has mostrado tu pensamiento en", count, "palabras!")
